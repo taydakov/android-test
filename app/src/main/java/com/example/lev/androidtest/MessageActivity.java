1f95b5a0fc1b9854242c8827aed13f9ba56c9f4e
Lev Taydakov <lev.taydakov@gmail.com>
2015-09-02 21:40:35 -0700

    Create new activity and start it on button click

--- app/src/main/java/com/example/lev/androidtest/MessageActivity.java
+++ app/src/main/java/com/example/lev/androidtest/MessageActivity.java
@@ -1,32 +1,22 @@
 package com.example.lev.androidtest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.lev.androidtest.EXTRA_MESSAGE";
-
-    private EditText messageEdit;
+public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        messageEdit = (EditText)findViewById(R.id.edit_message);
+        setContentView(R.layout.activity_message);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_message, menu);
         return true;
     }
 
@@ -43,14 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        String message = messageEdit.getText().toString();
-        Log.v("MainActivity", "Send message clicked, message = " + message);
-
-        Intent intent = new Intent(this, MessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
